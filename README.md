0791fd6983720d119df3e4d70fe40fab83f90f3e
Cory Tanner <ctannerweb@gmail.com>
2015-10-21 16:14:46 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Narwin Pack [![Build Status][ci-img]][ci]
+
+A collection of PostCSS plugins (DockYard)[https://dockyard.com] uses in our projects.
+
+## Installation
+```shell
+$npm install --save narwin-pack
+```
+
+## Using plugins
+- [autoprefixer](https://github.com/postcss/autoprefixer)
+- [postcss-partial-import](https://github.com/jonathantneal/postcss-partial-import),
+- [postcss-nested](https://github.com/postcss/postcss-nested),
+- [postcss-custom-properties](https://github.com/postcss/postcss-custom-properties),
+- [postcss-calc](https://github.com/postcss/postcss-calc)
+
+## Legal
+
+[DockYard](http://dockyard.com), Inc &copy; 2015
+
+[@dockyard](http://twitter.com/dockyard)
+
+[Licensed under the MIT license](http://www.opensource.org/licenses/mit-license.php)
---
+++ index.js
@@ -0,0 +1,48 @@
+var assign  = require('object-assign');
+var postcss = require('postcss');
+
+var processors = [
+  {
+    plugin: require('postcss-partial-import'),
+    namespace: 'import',
+    defaults: {}
+  },
+  {
+    plugin: require('postcss-nested'),
+    namespace: 'nested',
+    defaults: {}
+  },
+  {
+    plugin: require('postcss-custom-properties'),
+    namespace: 'properties',
+    defaults: {}
+  },
+  {
+    plugin: require('postcss-calc'),
+    namespace: 'calc',
+    defaults: {}
+  },
+  {
+    plugin: require('autoprefixer'),
+    defaults: { browsers: ['last 2 versions']}
+  }
+];
+
+module.exports = postcss.plugin('narwin-pack', function (opts) {
+  opts = assign({}, opts);
+
+  var instance = postcss();
+
+  processors.forEach(function (processor) {
+    var namespaceOptions = processor.namespace in opts ? opts[processor.namespace] : opts;
+    var processorOptions = {};
+
+    processorOptions = assign({}, processor.defaults, namespaceOptions);
+
+    if (namespaceOptions && !processorOptions.disable) {
+      instance.use(processor.plugin(processorOptions));
+    }
+  });
+
+  return instance;
+});
